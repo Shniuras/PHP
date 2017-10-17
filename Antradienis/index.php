e77c0b610089ae5997d76ef48345a4b41af0739d
Shniuras <seimurinho@gmail.com>
2017-10-17 15:36:04 +0300

    Antradienis

--- Antradienis/index.php
+++ Antradienis/index.php
@@ -11,12 +11,12 @@
 }
 suma($argv[1],$argv[2]);*/
 
-function getPrice (int $cost, int $vat){
+function get_price (int $cost, int $vat){
     $retailPrice = ($cost * 1.5) * (1+ $vat/100);
     echo $retailPrice;
 }
 
-function kuboTuris(int $a, int $b, int $c){
+function kubo_turis(int $a, int $b, int $c){
     $kuboTuris = $a * $b *$c;
     echo $kuboTuris;
 }
@@ -34,6 +34,110 @@ function draw($symbol, $rows, $cols){
             echo $symbol;
         }
         echo "\n";
+    }
+}
+
+function calc_wheel($RimWidth, $WheelWidth, $ArcWidth){
+    $RimResult = $RimWidth * 2.54;
+    $WheelResult = $WheelWidth/10*2;
+    $result = $RimResult+$WheelResult;
+
+    if($result < $ArcWidth){
+        echo "Padanga telpa i arka, nes VISOS padangos dydis yra " . $result . "CM" . " o arkos " . $ArcWidth . "CM";
+    } else {
+        echo "Padanga NETELPA, nes padangos dydis" . $result . "CM o arkos " . $ArcWidth . "CM";
+    }
+}
+
+function special_members() {
+    for ($i=0; $i<=20; $i++){
+        echo "\n";
+        echo $i . " " . $i%2;
+        if ($i%2){
+            echo "True";
+        }
+    }
+}
+
+function skaiciuok($nuo, $iki, $reiksme) {
+    if($reiksme == "true"){
+        for ($i=$nuo; $i<=$iki; $i++){
+            echo $i . "\n";
+        }
+    } elseif ($reiksme == "false") {
+        for ($i=$iki; $i>=$nuo; $i--){
+            echo $i . "\n";
+        }
+    }
+}
+
+function generate($num) {
+    $arr =[];
+    for($i=0; $i<=9; $i++){
+        array_push($arr, $num * $i);
+    }
+    print_r($arr);
+}
+
+function get_array(){
+    for($i=0; $i<=9; $i++){
+        generate($i);
+    }
+}
+
+function get_tanks($volume, $tankVolume) {
+    return ceil($volume/$tankVolume);
+}
+
+function get_volume($height, $width, $depth){
+    return $volume = $height*$width*$depth;
+}
+
+function random_numbers() {
+    for ($i=3; $i<=20; $i++){
+        echo get_volume($i, 20,2) . " " . get_tanks(get_volume($i, 20,2),200) ."\n";
+    }
+}
+
+function plyteles_plotas($plyteles_aukstis, $plyteles_plotis){
+    return $result = $plyteles_aukstis * $plyteles_plotis;
+}
+
+function baseino_plotas($x, $y, $z){
+    return $result = (2*$x*$z)+(2*$y*$z)+$x*$y;
+}
+
+function reikiamu_plyteliu_skaicius($x, $y, $z, $a, $b){
+    return $result = ceil(baseino_plotas($x,$y,$z)/plyteles_plotas($a,$b));
+}
+
+function jeigu_keiciasi_baseino_ilgis(){
+    for ($i=5.5; $i<=6.5; $i +=0.1){
+        echo $result = reikiamu_plyteliu_skaicius(2, $i, 3, 0.5, 0.3) . "\n";
+    }
+}
+
+function plyteliu_pakeliai() {
+    $pakelyje_plyteliu = 9;
+    for ($i=5.5; $i<=6.5; $i +=0.1){
+        $result = ceil(reikiamu_plyteliu_skaicius(2, $i, 3, 0.5, 0.3)/$pakelyje_plyteliu) . "\n";
+        echo $final= " Jeigu baseino ilgis bus " . $i . " tai reikes " . $result . "pakeliu plyteliu" . "\n";
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////
+/// MASYVAI
+
+$users = [
+    ["username" => "CoolPetras", "name"=>"Petras", "surname" => "Jonuska"],
+    ["username" => "Gamcius", "name"=>"Tadas", "surname" => "Ivanauskas"],
+    ["username" => "Juzepe", "name"=>"Juozas", "surname" => "Beluiskas"],
+    ["username" => "Serbas", "name"=>"Serbentautas", "surname" => "Uoga"],
+];
+
+function username() {
+    global $users;
+    foreach ($users as $user){
+        echo $user["username"]. "\n";
     }
 }
 
