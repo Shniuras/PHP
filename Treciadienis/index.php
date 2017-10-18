75aac93f510be6dcc5f0e60100ac6b977f43f592
Shniuras <seimurinho@gmail.com>
2017-10-18 14:34:04 +0300

    Treciadienis

--- Treciadienis/index.php
+++ Treciadienis/index.php
@@ -11,7 +11,7 @@
 }
 suma($argv[1],$argv[2]);*/
 
-function get_price (int $cost, int $vat){
+/*function get_price (int $cost, int $vat){
     $retailPrice = ($cost * 1.5) * (1+ $vat/100);
     echo $retailPrice;
 }
@@ -123,7 +123,7 @@ function plyteliu_pakeliai() {
         $result = ceil(reikiamu_plyteliu_skaicius(2, $i, 3, 0.5, 0.3)/$pakelyje_plyteliu) . "\n";
         echo $final= " Jeigu baseino ilgis bus " . $i . " tai reikes " . $result . "pakeliu plyteliu" . "\n";
     }
-}
+}*/
 ///////////////////////////////////////////////////////////////////////////////////////
 /// MASYVAI
 
@@ -140,7 +140,131 @@ function username() {
         echo $user["username"]. "\n";
     }
 }
-$fileName = array_shift($argv);
+
+function skaiciavimas($pirmas, $antras){
+
+//    $start = microtime();
+
+    for ($i=10; $i>0; $i--){
+        if($i==$pirmas || $i==$antras ){
+            for ($a=$i; $a>=0; $a--){
+                echo $a;
+            }
+            echo "\n";
+        } else {
+            echo $i . "\n";
+        }
+    }
+
+//    echo "Laikas buvo: " . (microtime() - $start);
+}
+
+function get_lowest(){
+    $temperatures = [28, 5, 3.5, 8, 6, 14, 20];
+    $lowest = $temperatures[0];
+
+    foreach ($temperatures as $t){
+        if ($t <$lowest){
+            $lowest = $t;
+        }
+    }
+    echo "Lowest temperature is: " . $lowest;
+}
+
+// Mano asmeninis pvz., kad geriau suprasciau Foreach loop'a
+function get_highest() {
+    $temperature = [15, 19, 18, 24, 21, 33, 28, 30];
+    foreach ($temperature as $temp){
+        $temp2 = rand(10,30);
+        if ($temp == $temp2){
+            echo "Laimejai" . " " . $temp . " " . $temp2 . "\n";
+        } else {
+            echo "Rezultatas: " . $temp . " " . $temp2 . "\n";
+        }
+    }
+}
+
+function dice($dices){
+    $game = [];
+    for ($i = 0; $i < $dices; $i++) {
+        //3 skirtingi budai kauliuku zaidimui sukurti
+        //1. array_push($game,rand(1, 6));
+        /*2. */
+        $game[$i] = rand(1, 6);
+        //3. $game[] = rand(1,6);
+        //Yra rand() ir yra mt_rand();
+    }
+    print_r($game);
+}
+
+//Uzduotis Nr.1
+// Funkcija turi rasyti, kad laimejai kai kauliuku skaiciai sutapo;
+
+// Destytojo parodytas atsakymas
+function uzduotis1_ats($dices){
+    $game = [];
+
+    for ($i=0; $i<$dices; $i++){
+        $game[$i] = mt_rand(1,6);
+    }
+    foreach ($game as $dice){
+        echo $dice . "\t";
+    }
+    foreach ($game as $dice) {
+        $ref = array_shift($game);
+        foreach ($game as $d){
+            if($ref == $d){
+                echo "Win!";
+                die();
+            }
+        }
+    }
+}
+
+// Uzduotis Nr.2
+// Jeigu iskrenta 3 ir 3 arba 4 ir 4 ir daugiau, tai reikia padauginti kauliuku isriedenta skaiciu is 100;
+function uzduotis2($dices){
+    $game = [];
+    $win = 0;
+
+    for ($i=0; $i<$dices; $i++){
+        $game[$i] = mt_rand(1,6);
+    }
+    foreach ($game as $dice){
+        echo $dice . "\t";
+    }
+    foreach ($game as $dice) {
+        $ref = array_shift($game);
+        foreach ($game as $d){
+            if($ref == $d){
+                if($ref>2){
+                    $win += ($ref*100);
+                }
+            }
+        }
+    }
+    echo $win;
+}
+
+//Uzduotis Nr.3
+// Isvesti kuri is temperaturu yra arciausiai 0
+
+function uzduotis3(){
+    $temperatures = [1, -2, 0.5, -1, -0.5, 5, -3];
+
+    $closest = $temperatures[0];
+
+    foreach ($temperatures as $t){
+        if(abs($t) < abs($closest)){
+            $closest = $t;
+        }
+    }
+    echo "Closest to 0 temperature is " . $closest;
+}
+
+
+
+array_shift($argv);
 //$argv yra pre-defined kintamasis;
 /*array_shift sumazina masyvo reiksmes is desine i kaire kaip pvz., $masyvas = array_shift[23,55,666];
 ...tai rezultatas gaunasi $masyvas = 23; ir musu masyvas dbr tapo [55,666];*/
